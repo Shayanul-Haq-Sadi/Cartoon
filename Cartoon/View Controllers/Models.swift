37c06b94cec79733301394b1ab92c116c5038234
Shayanul-Haq-Sadi <shayanul@braincraftapps.com>
2024-03-10 11:56:05 +0600

    Initial Commit

---
+++ Cartoon/View Controllers/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Cartoon
+//
+//  Created by BCL Device-18 on 7/3/24.
+//
+
+import Foundation
---
+++ Cartoon/Views/Cells/SmallBannerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SmallBannerCollectionViewCell.swift
+//  Cartoon
+//
+//  Created by BCL Device-18 on 6/3/24.
+//
+
+import UIKit
+
+class SmallBannerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
